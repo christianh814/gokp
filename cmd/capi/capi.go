de5c519a9dd6689ce12126e1c3fbbdf0355330bb
Christian Hernandez <chernand@redhat.com>
2021-09-26 12:27:15 -0700

    trying with server side apply

--- cmd/capi/capi.go
+++ cmd/capi/capi.go
@@ -1,9 +1,13 @@
 package capi
 
 import (
+	"io/ioutil"
 	"os"
 
 	//"github.com/aws/aws-sdk-go/aws"
+	"context"
+	"encoding/json"
+
 	"github.com/aws/aws-sdk-go/aws/session"
 	cfn "github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/christianh814/project-spichern/cmd/utils"
@@ -12,10 +16,24 @@ import (
 	cloudformation "sigs.k8s.io/cluster-api-provider-aws/cmd/clusterawsadm/cloudformation/service"
 	creds "sigs.k8s.io/cluster-api-provider-aws/cmd/clusterawsadm/credentials"
 	capiclient "sigs.k8s.io/cluster-api/cmd/clusterctl/client"
+
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/serializer/yaml"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/discovery"
+	"k8s.io/client-go/discovery/cached/memory"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
+	"k8s.io/client-go/tools/clientcmd"
 	//capiutil "sigs.k8s.io/cluster-api/util"
 )
 
 var CNIurl string = "https://docs.projectcalico.org/v3.20/manifests/calico.yaml"
+
+var decUnstructured = yaml.NewDecodingSerializer(unstructured.UnstructuredJSONScheme)
 
 // CreateAwsK8sInstance creates a Kubernetes cluster on AWS using CAPI and CAPI-AWS
 func CreateAwsK8sInstance(kindkconfig string, clusterName *string, workdir string, awscreds map[string]string) (bool, error) {
@@ -120,14 +138,16 @@ func CreateAwsK8sInstance(kindkconfig string, clusterName *string, workdir strin
 	//CHX
 	log.Info("Configuration complete, installing cluster")
 	//	use clientcmd to apply the configuration
-	/*
-		clusterInstallConfig, err := clientcmd.BuildConfigFromFlags("", kindkconfig)
-		if err != nil {
-			return false, err
-		}
+	clusterInstallConfig, err := clientcmd.BuildConfigFromFlags("", kindkconfig)
+	if err != nil {
+		return false, err
+	}
+	//clusterInstallClientSet, err := kubernetes.NewForConfig(clusterInstallConfig)
+	err = doSSA(context.TODO(), clusterInstallConfig, workdir+"/"+"install-cluster.yaml")
 
-		clusterInstallClientSet, err := kubernetes.NewForConfig(clusterInstallConfig)
-	*/
+	if err != nil {
+		return false, err
+	}
 
 	//clusterInstallClientSet.Result
 
@@ -143,4 +163,63 @@ func CreateAwsK8sInstance(kindkconfig string, clusterName *string, workdir strin
 	// If we're here, that means everything turned out okay
 	log.Info("Successfully created AWS Kubernetes Cluster")
 	return true, nil
+}
+
+func doSSA(ctx context.Context, cfg *rest.Config, yaml string) error {
+	// Read yaml into a slice of byte
+	yml, err := ioutil.ReadFile(yaml)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// get the RESTMapper for the GVR
+	dc, err := discovery.NewDiscoveryClientForConfig(cfg)
+	if err != nil {
+		return err
+	}
+	mapper := restmapper.NewDeferredDiscoveryRESTMapper(memory.NewMemCacheClient(dc))
+
+	// create dymanic client
+	dyn, err := dynamic.NewForConfig(cfg)
+	if err != nil {
+		return err
+	}
+
+	// read YAML manifest into unstructured.Unstructured
+	obj := &unstructured.Unstructured{}
+	_, gvk, err := decUnstructured.Decode(yml, nil, obj)
+	if err != nil {
+		return err
+	}
+
+	// Get the GVR
+	mapping, err := mapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	if err != nil {
+		return err
+	}
+
+	// Get the REST interface for the GVR
+	var dr dynamic.ResourceInterface
+	if mapping.Scope.Name() == meta.RESTScopeNameNamespace {
+		// namespaced resources should specify the namespace
+		dr = dyn.Resource(mapping.Resource).Namespace(obj.GetNamespace())
+	} else {
+		// for cluster-wide resources
+		dr = dyn.Resource(mapping.Resource)
+	}
+
+	// Create object into JSON
+	data, err := json.Marshal(obj)
+	if err != nil {
+		return err
+	}
+
+	// Create or Update the obj with service side apply
+	//     types.ApplyPatchType indicates service side apply
+	//     FieldManager specifies the field owner ID.
+	_, err = dr.Patch(ctx, obj.GetName(), types.ApplyPatchType, data, metav1.PatchOptions{
+		FieldManager: "gokp-bootstrapper",
+	})
+
+	return err
 }
