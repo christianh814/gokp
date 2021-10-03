116bb71f589b3a32b47bf09f134229671b7c0ea2
Christian Hernandez <chernand@redhat.com>
2021-10-02 19:41:20 -0700

    trying to export things

--- cmd/export/export.go
+++ cmd/export/export.go
@@ -60,12 +60,12 @@ func exportClusterScopedYaml(client *rest.Config, repodir string) (bool, error) 
 		// Get the config for a specific node
 		nd, err := c.CoreV1().Nodes().Get(context.TODO(), node.Name, metav1.GetOptions{})
 		if err != nil {
-			panic(err)
+			return false, err
 		}
 		//create the file
 		y, err := os.Create(repodir + "/" + "node-" + strings.ReplaceAll(nd.Name, ":", "-") + ".yaml")
 		if err != nil {
-			panic(err)
+			return false, err
 		}
 
 		// Make the YAML generic to store
@@ -79,9 +79,6 @@ func exportClusterScopedYaml(client *rest.Config, repodir string) (bool, error) 
 		nd.SetOwnerReferences([]metav1.OwnerReference{})
 		nd.SetGeneration(0)
 		nd.Status.Reset()
-		//needed for nodes maybe?
-		nd.Status.DaemonEndpoints.Reset()
-		nd.Status.NodeInfo.Reset()
 
 		// take the node and write the file, first adding any
 		// missing object information
@@ -90,13 +87,149 @@ func exportClusterScopedYaml(client *rest.Config, repodir string) (bool, error) 
 
 		err = e.Encode(nd, y)
 		if err != nil {
-			panic(err.Error())
+			return false, err
 		}
 		// close the file
 		y.Close()
 
 	}
 	// - END NODES -
+
+	// We get the PVS. Loop through each PV
+	pvs, _ := c.CoreV1().PersistentVolumes().List(context.TODO(), metav1.ListOptions{})
+	for _, pv := range pvs.Items {
+		// If we get nothing then don't bother, go to the next one
+		if len(pv.Name) == 0 {
+			continue
+		}
+
+		// Get the config for a specific pv
+		p, err := c.CoreV1().PersistentVolumes().Get(context.TODO(), pv.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		//create the file
+		y, err := os.Create(repodir + "/" + "pv-" + strings.ReplaceAll(p.Name, ":", "-") + ".yaml")
+		if err != nil {
+			return false, err
+		}
+
+		// Make the YAML generic to store
+		p.SetResourceVersion("")
+		p.SetUID("")
+		p.SetAnnotations(map[string]string{})
+		p.CreationTimestamp.Reset()
+		p.SetSelfLink("")
+		p.SetManagedFields([]metav1.ManagedFieldsEntry{})
+		p.SetFinalizers([]string{})
+		p.SetOwnerReferences([]metav1.OwnerReference{})
+		p.SetGeneration(0)
+		p.Status.Reset()
+
+		// take the node and write the file, first adding any
+		// missing object information
+		addTypeInformationToObject(p)
+		e := json.NewYAMLSerializer(json.DefaultMetaFactory, nil, nil)
+
+		err = e.Encode(p, y)
+		if err != nil {
+			return false, err
+		}
+		// close the file
+		y.Close()
+
+	}
+	// - END PVS -
+
+	// We get the Mutating Webhooks. Loop through each MW
+	mws, _ := c.AdmissionregistrationV1().MutatingWebhookConfigurations().List(context.TODO(), metav1.ListOptions{})
+	for _, mw := range mws.Items {
+		// If we get nothing then don't bother, go to the next one
+		if len(mw.Name) == 0 {
+			continue
+		}
+
+		// Get the config for a specific pv
+		m, err := c.AdmissionregistrationV1().MutatingWebhookConfigurations().Get(context.TODO(), mw.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		//create the file
+		y, err := os.Create(repodir + "/" + "pv-" + strings.ReplaceAll(m.Name, ":", "-") + ".yaml")
+		if err != nil {
+			return false, err
+		}
+
+		// Make the YAML generic to store
+		m.SetResourceVersion("")
+		m.SetUID("")
+		m.SetAnnotations(map[string]string{})
+		m.CreationTimestamp.Reset()
+		m.SetSelfLink("")
+		m.SetManagedFields([]metav1.ManagedFieldsEntry{})
+		m.SetFinalizers([]string{})
+		m.SetOwnerReferences([]metav1.OwnerReference{})
+		m.SetGeneration(0)
+
+		// take the node and write the file, first adding any
+		// missing object information
+		addTypeInformationToObject(m)
+		e := json.NewYAMLSerializer(json.DefaultMetaFactory, nil, nil)
+
+		err = e.Encode(m, y)
+		if err != nil {
+			return false, err
+		}
+		// close the file
+		y.Close()
+
+	}
+	// - END MWS -
+
+	// We get the validatingwebhookconfigurations loop through each
+	vwc, _ := c.AdmissionregistrationV1().ValidatingWebhookConfigurations().List(context.TODO(), metav1.ListOptions{})
+	for _, vw := range vwc.Items {
+		// If we get nothing then don't bother, go to the next one
+		if len(vw.Name) == 0 {
+			continue
+		}
+
+		// Get the config for a specific vw
+		v, err := c.AdmissionregistrationV1().ValidatingWebhookConfigurations().Get(context.TODO(), vw.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, err
+		}
+		//create the file
+		y, err := os.Create(repodir + "/" + "vwc-" + strings.ReplaceAll(v.Name, ":", "-") + ".yaml")
+		if err != nil {
+			return false, err
+		}
+
+		// Make the YAML generic to store
+		v.SetResourceVersion("")
+		v.SetUID("")
+		v.SetAnnotations(map[string]string{})
+		v.CreationTimestamp.Reset()
+		v.SetSelfLink("")
+		v.SetManagedFields([]metav1.ManagedFieldsEntry{})
+		v.SetFinalizers([]string{})
+		v.SetOwnerReferences([]metav1.OwnerReference{})
+		v.SetGeneration(0)
+
+		// take the node and write the file, first adding any
+		// missing object information
+		addTypeInformationToObject(v)
+		e := json.NewYAMLSerializer(json.DefaultMetaFactory, nil, nil)
+
+		err = e.Encode(v, y)
+		if err != nil {
+			return false, err
+		}
+		// close the file
+		y.Close()
+
+	}
+	// - END VWS -
 
 	// If we're here we should be okay
 	return true, nil
